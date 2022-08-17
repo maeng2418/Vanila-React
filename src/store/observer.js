fe287f143bb67d412a514faeab1a963df5cc563a
Maeng <maeng2418@naver.com>
2022-08-17 16:47:12 +0900

    [feat] Proxy 이용한 Observable 개선

--- src/store/observer.js
+++ src/store/observer.js
@@ -15,24 +15,20 @@ export const observe = (fn) => {
 };
 
 export const observable = (obj) => {
-  Object.keys(obj).forEach((key) => {
-    let _value = obj[key];
-    const observers = new Set();
+  const observerMap = {};
 
-    Object.defineProperty(obj, key, {
-      get() {
-        if (currentObserver) observers.add(currentObserver);
-        return _value;
-      },
-
-      set(value) {
-        // 상태가 변경되어 render를 해야하는데, 만약에 변경된 상태가 이전 상태와 값이 똑같을 경우
-        if (_value === value) return; // 숫자, 문자열, null, undefined 등의 원시타입
-        if (JSON.stringify(_value) === JSON.stringify(value)) return; // 배열이나 객체의 경우
-        _value = value;
-        observers.forEach((fn) => fn());
-      },
-    });
+  return new Proxy(obj, {
+    get(target, name) {
+      observerMap[name] = observerMap[name] || new Set();
+      if (currentObserver) observerMap[name].add(currentObserver);
+      return target[name];
+    },
+    set(target, name, value) {
+      if (target[name] === value) return true;
+      if (JSON.stringify(target[name]) === JSON.stringify(value)) return true;
+      target[name] = value;
+      observerMap[name].forEach((fn) => fn());
+      return true;
+    },
   });
-  return obj;
 };
