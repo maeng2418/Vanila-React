08022a4c7164cc187731a8c1a1bffc49e771ded4
Maeng <maeng2418@naver.com>
2022-08-17 15:01:04 +0900

    [feat] 전역상태관리 (Redux)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ObserverPage from "./pages/observer";
+import ReduxPage from "./pages/redux";
 import VuexPage from "./pages/vuex";
 import WebComponentPage from "./pages/web-component";
 import Component from "./utils/component";
@@ -17,6 +18,7 @@ class App extends Component {
       <button id="webComponent">웹 컴포넌트 만들기</button>
       <button id="observer">옵져버 상태관리 시스템 만들기</button>
       <button id="vuex">Flux 상태관리 시스템 만들기 (Vuex)</button>
+      <button id="redux">Flux 상태관리 시스템 만들기 (Redux)</button>
       <div id="container"></div>
     `;
   }
@@ -29,6 +31,7 @@ class App extends Component {
     if (page === "/webComponent") new WebComponentPage($container);
     if (page === "/observer") new ObserverPage($container);
     if (page === "/vuex") new VuexPage($container);
+    if (page === "/redux") new ReduxPage($container);
   }
 
   setEvent() {
@@ -44,6 +47,10 @@ class App extends Component {
 
     $target.querySelector("#vuex").addEventListener("click", () => {
       state.page = "/vuex";
+    });
+
+    $target.querySelector("#redux").addEventListener("click", () => {
+      state.page = "/redux";
     });
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 export { observable, observe } from "./observer";
 export { default as ObserverStore } from "./observerStore";
 export { default as Publisher } from "./publisher";
+export { default as createStore } from "./redux";
 export { default as Subscriber } from "./subscriber";
 export { default as Vuex } from "./vuex";
