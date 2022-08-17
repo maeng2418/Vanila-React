02df9c9da42b8081beb0b28ec4b7ef2ee9599a64
Maeng <maeng2418@naver.com>
2022-08-17 16:39:19 +0900

    [feat] 리렌더링 방어 로직

--- src/store/observer.js
+++ src/store/observer.js
@@ -18,6 +18,9 @@ export const observable = (obj) => {
       },
 
       set(value) {
+        // 상태가 변경되어 render를 해야하는데, 만약에 변경된 상태가 이전 상태와 값이 똑같을 경우
+        if (_value === value) return; // 숫자, 문자열, null, undefined 등의 원시타입
+        if (JSON.stringify(_value) === JSON.stringify(value)) return; // 배열이나 객체의 경우
         _value = value;
         observers.forEach((fn) => fn());
       },
